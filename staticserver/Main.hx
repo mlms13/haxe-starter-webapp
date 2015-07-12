14f19551dae13d5ac00ef4e65da06a15655b35ec
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-07-11 23:24:28 -0600

    Actually make livereload work

--- staticserver/Main.hx
+++ staticserver/Main.hx
@@ -1,8 +1,10 @@
 import abe.App;
+import mw.ConnectLivereload;
 
 class Main {
   public static function main() {
     var app = new App();
+    app.router.use(ConnectLivereload.create());
     app.router.serve('/', './public');
     app.http(3333);
   }
